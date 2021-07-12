b96c309e5c67d455a0f2cb43e1a6e0f91f1e2f81
Telemonic <matt24241999@gmail.com>
2021-07-12 17:38:45 -0400

    v2.1 Update

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const client = new Discord.Client();
 globalDebug = true; //Prints otuput to console
 
 //Variables
-var maxPlayers = 8; //Total Max Players (i.e. 8 players means 4 vs 4)
+var maxPlayers = config.maxplayers; //Total Max Players (i.e. 8 players means 4 vs 4)
 var playerCount = 0; //Current player count
 var mapsVoteCount = 0; //tracks amount of Votes for map voting
 var selectedMap = "";
@@ -19,10 +19,16 @@ var captain2; //Captain 2
 var c2PickCount = 0; //Number of players picked by captain 2
 var c1Array = [maxPlayers/2] // Array of players for team captain 1
 var c2Array = [maxPlayers/2] // Array of players for team captain 2
-var currentlyPicking = "";
-var c1StartSide = "";
-var c2StartSide = "";
-var adminRole;
+var survivorArray = []; //array for survivors
+var infectedArray = []; //array for infected
+var currentlyPicking = ""; //holds currently picking players discord ID
+var adminRole = config.adminID; //ID for admin role
+var lobbyID = config.lobbyID; //ID for lobby VC
+var serverIP = config.defaultServerID; //default server ip from config
+var mapTimer; // Timer object to end voting automatically after value specified in mapVoteTime
+var mapVoteTime = config.mapVoteTime * 1000; //time before voting automatically ends (in ms)
+var pugChannelID = config.pugChannelID; //ID for the pug channel (channel bot commands are locked to
+
 //Arrays
 playerArray = [maxPlayers]; //initializes playerArray
 mapsArray = config.maps; //sets mapsArray to the maps defined in config.json
@@ -38,19 +44,25 @@ phaseSidePick = false; //picks starting side
 phasePugStart = false; //starts pug
 
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log('---------------------');
+	console.log('PUGbot - by Telemonic');
+    console.log('---------------------');
 });
 
 client.on('message', message => {
-    //Role Getters
 
-    //checks for prefix
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+	//azure is an idiot
+	if (message.author.id === '72559613681078272') {
+		message.react('862066693978587216');
+	}
 
-	//Ignores DMs
-	if (message.channel instanceof Discord.DMChannel) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return; //checks for prefix
 
-    cPrint("Command was recieved by " + message.author.username + ": " + message.content);
+	if (message.channel instanceof Discord.DMChannel) return; //Ignores DMs
+
+    if (message.channel.id != pugChannelID) return; //Ignores Messages from other channels.
+
+    cPrint("Command was recieved by " + message.author.username + ": " + message.content); //prints output to console
 
     //splices message content
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
@@ -69,17 +81,24 @@ client.on('message', message => {
                 message.channel.send("There is already a PUG open!");
             }
         }else{
-            message.channel.send("You are not an admin!");
+            message.channel.send("Insufficient Permissions.");
         }
     }
     //add up to pug command
     else if (command === 'add'){
         addPlayer(message, message.author.id);
     }
+    else if(command == 'test'){
+
+    }
     //force adds a user (!forceadd @user)
-    else if (command === 'forceadd'){
-        if(args.length){
-            addPlayer(message, trimID(args[0]));
+    else if (command === 'forceadd' || command === 'f'){
+        if(checkAdmin(message)){
+            if(args.length){
+                addPlayer(message, trimID(args[0]));
+            }
+        }else{
+            message.channel.send("Insufficient Permissions.");
         }
     }
     //removes message sender
@@ -89,7 +108,9 @@ client.on('message', message => {
     //kicks a user (!kick @user)
     else if(command === 'kick'){
         if(args.length){
-            removePlayer(message, trimID(args[0]));
+            if(checkAdmin(message)){
+                removePlayer(message, trimID(args[0]));
+            }
         }else{
             message.channel.send("Invalid syntax.")
         }
@@ -100,6 +121,16 @@ client.on('message', message => {
             if(playerCount == maxPlayers){
                 phaseMapVote = true;
                 phasePugOpen = false;
+                
+                dmPlayers(playerArray, message);
+
+                mapTimer = setTimeout(function(){getMap(message); }, mapVoteTime); //timer to auto end voting 
+                mapVoteQuarter = mapVoteTime/4;
+
+                message.channel.send(mapVoteTime/4000 + " seconds remaining.");
+                timer75 = setTimeout(function(){message.channel.send(mapVoteTime/4000*3 + " seconds remaining."); }, mapVoteQuarter);
+                timer50 = setTimeout(function(){message.channel.send(mapVoteTime/4000*2 + " seconds remaining."); }, mapVoteQuarter * 2);
+                timer25 = setTimeout(function(){message.channel.send(mapVoteTime/4000*1 + " seconds remaining."); }, mapVoteQuarter * 3);
 
                 for(var i = 0; i < mapsArray.length; i++){
                     mapVotingArrayMaps[i] = 0;
@@ -112,17 +143,27 @@ client.on('message', message => {
     }
     //map voting command
     else if (command === 'vote'){
-
         voteMap(message, args[0]);
+    }
+    else if (command === 'endvoting'){
+        if(phaseMapVote == true){
+            if(checkAdmin(message)){
+                getMap(message);
+            }
+        }
     }
     //captain delcare command
     else if(command === 'captains'){
-        if(args.length){
-            if(args.length == 2){
-				if(checkAdded(trimID(args[0]), playerArray) && checkAdded(trimID(args[1]), playerArray)){
-					pickCaptain(message, args[0], args[1]);
-				}
+        if(checkAdmin(message)){
+            if(args.length){
+                if(args.length == 2){
+                    if(checkAdded(trimID(args[0]), playerArray) && checkAdded(trimID(args[1]), playerArray)){
+                        pickCaptain(message, args[0], args[1]);
+                    }
+                }
             }
+        }else{
+            message.channel.send("Insufficient Permissions.");
         }
     }
     else if (command === 'pick'){
@@ -153,41 +194,56 @@ client.on('message', message => {
     }
     else if (command === 'side'){
         if(args.length){
-            if(args[0] == 'infected' || args[0] == 'survivor'){
-                if(args[0] == 'infected'){
-                    c2StartSide = 'infected';
-                    c1StartSide = 'survivor';
-                }else{
-                    c1StartSide = 'infected';
-                    c2StartSide = 'survivor';
-                }
-                phaseSidePick = false;
-                phasePugStart = true;
+            if(message.author.id == captain2){
+                if(args[0] == 'infected' || args[0] == 'survivor'){
+                    if(args[0] == 'infected'){
+                        infectedArray = c2Array;
+                        survivorArray = c1Array;
+                    }else{
+                        survivorArray = c2Array;
+                        infectedArray = c1Array;
+                    }
+                    phaseSidePick = false;
+                    phasePugStart = true;
+                }   
             }
+        }
+    }
+    else if (command === 'server'){
+        if(checkAdmin(message)){
+            if(args.length){
+                serverIP = args[0];
+                message.channel.send("Server IP has been changed to: " + args[0]);
+            }
+        }else{
+            message.channel.send ("Insifficient Permissions.");
         }
     }
     else if (command === 'help'){
         printHelp(message);
     }
     else if (command === 'maxplayers'){
-        if(args.length){
-            if(Number.isInteger(parseInt(args[0]))){
-                resetPUG();
-                maxPlayers = args[0];
-                message.channel.send("Max players has been changed to " + maxPlayers + ".");
-            }else{
-                message.channel.send("Please enter a valid integer.");
+        if(checkAdmin(message)){
+            if(args.length){
+                if(Number.isInteger(parseInt(args[0]))){
+                    resetPUG();
+                    maxPlayers = args[0];
+                    message.channel.send("Max players has been changed to " + maxPlayers + ".");
+                }else{
+                    message.channel.send("Please enter a valid integer.");
+                }
             }
         }
     }
 	else if (command === 'endpug'){
-		resetPUG();
-		message.channel.send("PUG has been ended.");
-	}
+        if(checkAdmin(message)){
+            resetPUG();
+            message.channel.send("PUG has been ended.");
+	    }
+    }   
     //checks if pug is filled
     if(phasePugOpen == true){
         if (playerCount == maxPlayers){
-			adminRole = message.guild.roles.cache.find(role => role.name === "Pug Admin");
             message.channel.send(pingRole(adminRole) + "The PUG has reached " + playerCount + " / " + maxPlayers + " players and is ready to !start");
         }
     }else if(phaseMapVote == true){
@@ -196,7 +252,6 @@ client.on('message', message => {
         }
     //checks for captain pick phase
     }if(phaseCaptainPick == true){
-		adminRole = message.guild.roles.cache.find(role => role.name === "Pug Admin");
         message.channel.send(pingRole(adminRole) + " Declare two captains by doing !captains @Captain1 @Captain2");
     }
     if(phasePlayerPick == true){
@@ -210,10 +265,6 @@ client.on('message', message => {
         phasePugStart = false;
         resetPUG();
     }
-	//azure is an idiot
-	if (message.author.id === '72559613681078272') {
-		message.react('862066693978587216');
-	}
 });
 
 //functions
@@ -257,13 +308,29 @@ function getUsername(userID){
 	return username;
 }
 function checkAdmin(message){
-    if(message.member.roles.cache.some(role => role.name === 'Pug Admin') || message.member.roles.cache.some(role => role.name === 'Server Admin')){
+    if(message.member.roles.cache.some(role => role.id === adminRole)){
         return true;
     }else{
         return false;
     }
 }
 function getList(array){
+	var list = "";
+    if(array.length > 0){
+        for(var i = 0; i < array.length; i++){
+            if(array[i] != null && array[i] != ""){
+    
+                list += (pingUser(array[i]));
+                list += "\n";
+            }
+        }
+        if(list.length == 0){
+                list += " ";
+        }
+        return list;
+    }
+}
+function getListNum(array){
 	var list = "";
     if(array.length > 0){
         for(var i = 0; i < array.length; i++){
@@ -292,7 +359,7 @@ function addPlayer(message, id){
                 .setThumbnail(client.guilds.resolve(message.guild.id).members.resolve(id).user.avatarURL())
                 .addFields(
                     {name: 'Currently added players:',
-                    value:getList(playerArray)})
+                    value:getListNum(playerArray)})
                 message.channel.send(embed);
 				client.user.setActivity(`L4D2 PUGs - (${playerCount}` + "/"+ `${maxPlayers}) !add`); 
             }else{
@@ -318,7 +385,7 @@ function removePlayer(message, id){
             .setThumbnail(client.guilds.resolve(message.guild.id).members.resolve(id).user.avatarURL())
             .addFields(
                 {name: 'Currently added players:',
-                value:getList(playerArray)})
+                value:getListNum(playerArray)})
             message.channel.send(embed);
 			client.user.setActivity(`L4D2 PUGs - (${playerCount}` + "/"+ `${maxPlayers}) !add`); 
         }else{
@@ -346,8 +413,14 @@ function printMaps(message){
 }
 function printMapsMessage(message, voteArray){
     var mapsList = "";
+    var bold = "";
+
     for(var i = 0; i < mapsArray.length; i++){
-        mapsList += `${i+1}) ${mapsArray[i]}: ${voteArray[i]} votes \n`;
+        if(mapsArray[i] == selectedMap){
+            bold = "*";
+        }
+        mapsList += `${bold}${i+1}) ${mapsArray[i]}: ${voteArray[i]} votes${bold}\n`;
+        bold = "";
     }	
     return mapsList;
 }
@@ -362,7 +435,7 @@ function voteMap(message, vote){
                         mapVotingArrayMaps[vote-1]++;
                         mapsVoteCount++;
                         
-                        //adds player to the array to stop dupe voting
+                        //adds player to the array to stop dupe voting (Validate Vote)
                         mapVotingArrayPlayers[mapsVoteCount] = message.author.id;
 
                     }   
@@ -384,6 +457,11 @@ function getMap(message){
     var maxIndex = 0;
     var mapsList = printMapsMessage(message, mapVotingArrayMaps);
 
+    clearTimeout(mapTimer);
+    clearTimeout(timer75);
+    clearTimeout(timer50);
+    clearTimeout(timer25);
+
     for(var i = 0; i < mapsArray.length; i++){
         if(mapVotingArrayMaps[i] > max){
             max = mapVotingArrayMaps[i];
@@ -393,11 +471,10 @@ function getMap(message){
 
     selectedMap = mapsArray[maxIndex];
     mapIMG = mapsArray[maxIndex];
-    mapIMG = mapIMG.replace(/\s+/g, '');
-
+    mapIMG = replaceMap(mapIMG)
     const embed = new Discord.MessageEmbed()
 
-    .setTitle('Map Voting - ' + mapsArray[maxIndex] + " won with " + mapVotingArrayMaps[maxIndex] + " / " + maxPlayers + " votes")
+    .setTitle('Map Voting - ' + mapsArray[maxIndex] + " won with " + mapVotingArrayMaps[maxIndex] + " / " + mapsVoteCount + " votes")
     .setColor('#DAF7A6')
     .attachFiles([`./images/${mapIMG}.png`])
     .setThumbnail(`attachment://${mapIMG}.png`)
@@ -455,7 +532,7 @@ function printPickPhase(message){
     .setThumbnail(client.guilds.resolve(message.guild.id).members.resolve(currentlyPicking).user.avatarURL())
     .addFields(
     {name: 'Currently Available Players (!pick #)',
-    value:`${getList(playerArray)}`})
+    value:`${getListNum(playerArray)}`})
 
     message.channel.send(embed);
 }
@@ -474,27 +551,25 @@ function getPickedUser(index){
     return playerArray[index - 1];
 }
 function printMatch(message){
-    var currentdate = new Date(); 
-    var date = (currentdate.getMonth()+1) + "/"
-            +  currentdate.getDate() + "/" 
-            + currentdate.getFullYear();
+
+    var date = getDate();
 
     const embed = new Discord.MessageEmbed()
         
     .setTitle("PUG - " + selectedMap)
     .setColor('#DAF7A6')
-    .attachFiles([`./images/${mapIMG}.png`])
+    .attachFiles([`./images/${replaceMap(selectedMap)}.png`])
     .setFooter("             " + date)
-    .setThumbnail(`attachment://${mapIMG}.png`)
+    .setThumbnail(`attachment://${replaceMap(selectedMap)}.png`)
     .addFields(
 		{name: 'Survivor',
-		value:`${getList(c1Array)}`,
+		value:`${getList(survivorArray)}`,
 		inline: true},
 		{name: 'Infected',
-		value:`${getList(c2Array)}`,
+		value:`${getList(infectedArray)}`,
 		inline: true},
 		{name: 'Connect Info:',
-		value:'steam://connect/74.91.124.174',
+		value:'steam://connect/'+serverIP,
 		inline: false})
 
     message.channel.send(embed);
@@ -511,9 +586,9 @@ function resetPUG(){
     c2PickCount = 0; //Number of players picked by captain 2
     c1Array = [maxPlayers/2] // Array of players for team captain 1
     c2Array = [maxPlayers/2] // Array of players for team captain 2
+    survivorArray = [];
+    infectedArray = [];
     currentlyPicking = "";
-    c1StartSide = "";
-    c2StartSide = "";
 
     //Arrays
     playerArray = [maxPlayers]; //initializes playerArray
@@ -529,6 +604,9 @@ function resetPUG(){
     phaseSidePick = false; //picks starting side
     phasePugStart = false; //starts pug
 }
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
 function printHelp(message){
     client.users.fetch(message.author.id, false).then((user) => {
         user.send(`**PUGBot v2.0 - By Telemonic**
@@ -539,8 +617,9 @@ Admin Commands:
     - ${prefix}kick: *Kicks a user from the PUG.*
     - ${prefix}start: *Starts the PUG once the player cap is reached.*
     - ${prefix}captains: *Declares the two team captains for the PUG (Usage: captains @captain1 @captain2)*
-    - ${prefix}maxplayers: *Changes the max players to the value specified. (Warning: Will reset current PUG)*
+    - ${prefix}maxplayers: *Changes the max players to the value specified. (Warning: Will reset current PUG.)*
     - ${prefix}endpug: ends the current PUG.
+    - ${prefix}endvoting: *Ends the voting early and uses the current votes*
 
 Player Commands:
     - ${prefix}add: *Adds you to the current PUG.*
@@ -549,11 +628,64 @@ Player Commands:
     - ${prefix}pick: *Picks a user for your team, if you are a team captain (Usage: pick #)*
     - ${prefix}side: *Picks either infected or survivor start if you are prompted.*
     - ${prefix}help: *Prints this message.*
+    
         
 Github link: *https://github.com/telemonic/PUGbot*
         `);
        });
 }
+function shuffle(array) {
+    var currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+}
+function pingChannel(channel){
+    return "<#"+channel+">";
+}
+function replaceMap(map){
+    return map.replace(/\s+/g, '');
+}
+function dmPlayers(array, message){
+    for(var i = 0; i < maxPlayers; i++){
+        client.users.fetch(array[i], false).then((user) => {
+            user.send(dmPlayersMessage(message));
+        });
+    }
+}
+function getDate(){
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy;
+    return today;
+}
+function dmPlayersMessage(message){
+    const embed = new Discord.MessageEmbed()
+    .setTitle("The PUG has filled! (" + playerCount + " / " + maxPlayers + ")")
+    .setColor('#DAF7A6')
+    .addFields(
+        {name: 'Connect here:',
+        value:pingChannel(lobbyID)},
+        {name: 'Players:',
+        value:getList(playerArray)}
+        )
+    return embed;
+}
+
+
 client.login(token);
 
 
